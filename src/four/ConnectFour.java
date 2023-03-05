36b46af376d7396e48acd285c06f74fb57b13cb2
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-03-05 09:29:03 +0100

    {wip}

--- src/four/ConnectFour.java
+++ src/four/ConnectFour.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 public class ConnectFour extends JFrame {
     private String player = "X";
     public ConnectFour() {
@@ -18,12 +20,13 @@ public class ConnectFour extends JFrame {
     }
     public void initComponents() {
 
-        JPanel panel = new JPanel(new GridLayout(6, 7, 5, 5));
+        JPanel panel = new JPanel(new GridLayout(6, 7));
         List<JButton> buttons = new ArrayList<>();
         for (int j = 6; j >= 1 ; j--) {
         for (char i = 'A'; i <= 'G'; i++) {
                 JButton button = new JButton(" ");
                 button.setName("Button" + i + j);
+                button.setBackground(Color.LIGHT_GRAY);
                 button.setFocusPainted(false);
                 panel.add(button);
                 buttons.add(button);
@@ -33,7 +36,20 @@ public class ConnectFour extends JFrame {
             Writer(i, buttons);
         }
 
-        add(panel);
+        JButton ButtonReset = new JButton("Reset");
+        ButtonReset.setName("ButtonReset");
+        JPanel panel2 = new JPanel(new BorderLayout());
+        panel2.add(ButtonReset, BorderLayout.EAST);
+        add(panel, BorderLayout.CENTER);
+        add(panel2, BorderLayout.SOUTH);
+
+        ButtonReset.addActionListener(e -> {
+            for (JButton button : buttons
+                 ) {
+                button.setText(" ");
+                button.setBackground(Color.LIGHT_GRAY);
+            }
+        });
 
     }
     public void Writer(int begin, List<JButton> buttons) {
@@ -45,13 +61,62 @@ public class ConnectFour extends JFrame {
 
         }
     }
+    public static String[][] ArrayToMatrixConverter(List<JButton> array) {
+        String[][] matrix = new String[6][7];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                matrix[i][j] = array.get(j + i * 7).getText();
+            }
+        }
+        return matrix;
+    }
+    public static boolean IsWinner(String var, List<JButton> buttons) {
+        String[][] matrix = ArrayToMatrixConverter(buttons);
+        boolean isWin = false;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (i >= 3) {
+                    if (Objects.equals(matrix[i][j], var) && Objects.equals(matrix[i - 1][j], var) &&
+                            Objects.equals(matrix[i - 2][j], var) && Objects.equals(matrix[i - 3][j], var)){
+                        isWin = true;
+
+                    }
+                }
+                if (i >= 3 && j >= 3) {
+                    if ((Objects.equals(matrix[i][j], var) && Objects.equals(matrix[i - 1][j - 1], var) &&
+                                    Objects.equals(matrix[i - 2][j - 2], var) && Objects.equals(matrix[i - 3][j - 3], var))
+                    || (Objects.equals(matrix[i - 3][j], var) && Objects.equals(matrix[i - 2][j - 1], var) &&
+                            Objects.equals(matrix[i - 1][j - 2], var) && Objects.equals(matrix[i][j - 3], var))) {
+                        isWin = true;
+
+                    }
+                }
+                if (j >= 3) {
+                    if (Objects.equals(matrix[i][j], var) && Objects.equals(matrix[i][j - 1], var) &&
+                            Objects.equals(matrix[i][j - 2], var) && Objects.equals(matrix[i][j - 3], var)){
+                        isWin = true;
+
+                    }
+                }
+            }
+        }
+        return isWin;
+    }
     public void FreeRow(int begin, List<JButton> buttons) {
             for (int k = begin; k >= 0 ; k -= 7) {
                 if (buttons.get(k).getText().equals(" ")) {
                     buttons.get(k).setText(this.player);
+                    if (IsWinner(this.player, buttons)) {
+                        for (JButton button : buttons
+                        ) {
+                            button.setBackground(Color.BLUE);
+                            button.setEnabled(false);
+                        }
+                    }
                     break;
                 }
             }
+
     }
     public static void main(String[] args) {
         new ConnectFour();
